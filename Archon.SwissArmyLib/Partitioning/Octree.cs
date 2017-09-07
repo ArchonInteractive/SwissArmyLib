9db7feb95783c9a2f5e855d426eafa483aab16dd
Casper Christiansen <casper.developer@gmail.com>
2017-09-07 14:33:03 +0200

    Added octree

--- Archon.SwissArmyLib/Partitioning/Octree.cs
+++ Archon.SwissArmyLib/Partitioning/Octree.cs
@@ -6,39 +6,39 @@ using UnityEngine;
 namespace Archon.SwissArmyLib.Partitioning
 {
     /// <summary>
-    /// A GC-friendly <see href="https://en.wikipedia.org/wiki/Quadtree">Quadtree</see> implementation.
+    /// A GC-friendly <see href="https://en.wikipedia.org/wiki/Octree">Octree</see> implementation.
     /// 
     /// Use the static <see cref="Create"/> and <see cref="Destroy"/> methods for creating and destroying trees.
     /// If you forget to <see cref="Destroy"/> a tree when you're done with it, it will instead be collected 
     /// by the GC as normal, but the nodes will not be recycled.
     /// </summary>
-    /// <typeparam name="T">The type of items this quadtree should hold.</typeparam>
-    public class Quadtree<T> : IPoolable, IDisposable
+    /// <typeparam name="T">The type of items this octree should hold.</typeparam>
+    public class Octree<T> : IPoolable, IDisposable
     {
         // We can't use PoolHelper since the constructor is private
-        private static readonly Pool<Quadtree<T>> Pool = new Pool<Quadtree<T>>(() => new Quadtree<T>());
+        private static readonly Pool<Octree<T>> Pool = new Pool<Octree<T>>(() => new Octree<T>());
 
         /// <summary>
-        /// Creates a Quadtree.
+        /// Creates an Octree.
         /// </summary>
         /// <param name="bounds">The size of the tree's bounds.</param>
         /// <param name="maxItems">The amount of items a node can contain before splitting.</param>
         /// <param name="maxDepth">The maximum depth</param>
-        /// <returns>The quadtree.</returns>
-        public static Quadtree<T> Create(Rect bounds, int maxItems, int maxDepth)
+        /// <returns>The octree.</returns>
+        public static Octree<T> Create(Bounds bounds, int maxItems, int maxDepth)
         {
-            var quadtree = Pool.Spawn();
-            quadtree.Bounds = bounds;
-            quadtree.MaxItems = maxItems;
-            quadtree.MaxDepth = maxDepth;
-            return quadtree;
+            var tree = Pool.Spawn();
+            tree.Bounds = bounds;
+            tree.MaxItems = maxItems;
+            tree.MaxDepth = maxDepth;
+            return tree;
         }
 
-        private static Quadtree<T> CreateSubtree(Rect bounds, int maxItems, int depth, int maxDepth)
+        private static Octree<T> CreateSubtree(Bounds bounds, int maxItems, int depth, int maxDepth)
         {
-            var quadtree = Create(bounds, maxItems, maxDepth);
-            quadtree.Depth = depth;
-            return quadtree;
+            var tree = Create(bounds, maxItems, maxDepth);
+            tree.Depth = depth;
+            return tree;
         }
 
         /// <summary>
@@ -47,7 +47,7 @@ namespace Archon.SwissArmyLib.Partitioning
         /// Do not use the tree again after calling this.
         /// </summary>
         /// <param name="tree">The tree to destroy.</param>
-        public static void Destroy(Quadtree<T> tree)
+        public static void Destroy(Octree<T> tree)
         {
             Pool.Despawn(tree);
         }
@@ -55,30 +55,30 @@ namespace Archon.SwissArmyLib.Partitioning
         private struct ItemBounds
         {
             public readonly T Item;
-            public readonly Rect Bounds;
+            public readonly Bounds Bounds;
 
-            public ItemBounds(T item, Rect bounds)
+            public ItemBounds(T item, Bounds bounds)
             {
                 Item = item;
                 Bounds = bounds;
             }
         }
 
-        private readonly Quadtree<T>[] _subNodes = new Quadtree<T>[4];
+        private readonly Octree<T>[] _subNodes = new Octree<T>[8];
         private readonly List<ItemBounds> _items = new List<ItemBounds>();
 
         /// <summary>
-        /// Gets the bounds of this quadtree node.
+        /// Gets the bounds of this octree node.
         /// </summary>
-        public Rect Bounds { get; private set; }
+        public Bounds Bounds { get; private set; }
 
         /// <summary>
-        /// Gets the depth of this quadtree node.
+        /// Gets the depth of this octree node.
         /// </summary>
         public int Depth { get; private set; }
 
         /// <summary>
-        /// Gets the maximum depth that the quadtree can go.
+        /// Gets the maximum depth that the octree can go.
         /// </summary>
         public int MaxDepth { get; private set; }
 
@@ -97,17 +97,17 @@ namespace Archon.SwissArmyLib.Partitioning
         /// </summary>
         public int Count { get; private set; }
 
-        private Quadtree()
+        private Octree()
         {
 
         }
 
         /// <summary>
-        /// Inserts an item with the specified bounds into the quadtree.
+        /// Inserts an item with the specified bounds into the octree.
         /// </summary>
         /// <param name="item">The item to insert.</param>
         /// <param name="bounds">The bounds of the item, used to place it correctly in the tree.</param>
-        public void Insert(T item, Rect bounds)
+        public void Insert(T item, Bounds bounds)
         {
             Count++;
 
@@ -138,12 +138,12 @@ namespace Archon.SwissArmyLib.Partitioning
         }
 
         /// <summary>
-        /// Removes an item with the specified bounds from the quadtree.
+        /// Removes an item with the specified bounds from the octree.
         /// </summary>
         /// <param name="item">The item to remove.</param>
         /// <param name="bounds">The bounds used for previously inserting this item.</param>
         /// <returns>True if item was found and removed, otherwise false.</returns>
-        public bool Remove(T item, Rect bounds)
+        public bool Remove(T item, Bounds bounds)
         {
             if (IsSplit)
             {
@@ -183,37 +183,37 @@ namespace Archon.SwissArmyLib.Partitioning
         }
 
         /// <summary>
-        /// Retrieves all potential matches for the given <see cref="Rect"/>.
+        /// Retrieves all potential matches for the given <see cref="Bounds"/>.
         /// 
         /// Careful, this method creates a new <see cref="HashSet{T}"/>. 
-        /// You might want to use <see cref="Retrieve(Rect, HashSet{T})"/> instead if you call this often.
+        /// You might want to use <see cref="Retrieve(Bounds, HashSet{T})"/> instead if you call this often.
         /// </summary>
-        /// <param name="rect"></param>
+        /// <param name="bounds"></param>
         /// <returns>Potential matches.</returns>
-        public HashSet<T> Retrieve(Rect rect)
+        public HashSet<T> Retrieve(Bounds bounds)
         {
             var results = new HashSet<T>();
-            Retrieve(rect, results);
+            Retrieve(bounds, results);
             return results;
         }
 
         /// <summary>
-        /// Retrieves all potential matches for the given <see cref="Rect"/> and adds them to <paramref name="results"/>.
+        /// Retrieves all potential matches for the given <see cref="Bounds"/> and adds them to <paramref name="results"/>.
         /// </summary>
-        /// <param name="rect"></param>
+        /// <param name="bounds"></param>
         /// <param name="results">Where results will be added to.</param>
-        public void Retrieve(Rect rect, HashSet<T> results)
+        public void Retrieve(Bounds bounds, HashSet<T> results)
         {
             if (IsSplit)
             {
-                var containingNodes = GetContainingNodes(rect);
+                var containingNodes = GetContainingNodes(bounds);
                 if (containingNodes != SubNodes.None)
                 {
                     for (var i = 0; i < _subNodes.Length; i++)
                     {
                         var subNode = 1 << i;
                         if (((byte)containingNodes & subNode) == subNode)
-                            _subNodes[i].Retrieve(rect, results);
+                            _subNodes[i].Retrieve(bounds, results);
                     }
                 }
             }
@@ -241,7 +241,7 @@ namespace Archon.SwissArmyLib.Partitioning
         }
 
         /// <summary>
-        /// Destroys the quadtree using <see cref="Destroy"/>.
+        /// Destroys the octree using <see cref="Destroy"/>.
         /// 
         /// Do not use the tree after calling this!
         /// </summary>
@@ -252,18 +252,26 @@ namespace Archon.SwissArmyLib.Partitioning
 
         private void Split()
         {
-            var bounds = Bounds;
-            var subNodeSize = bounds.size * 0.5f;
-            var topLeft = new Rect(bounds.xMin, bounds.center.y, subNodeSize.x, subNodeSize.y);
-            var topRight = new Rect(bounds.center, subNodeSize);
-            var bottomLeft = new Rect(bounds.min, subNodeSize);
-            var bottomRight = new Rect(bounds.center.x, bounds.yMin, subNodeSize.x, subNodeSize.y);
-
             var newDepth = Depth + 1;
-            _subNodes[0] = CreateSubtree(topLeft, MaxItems, newDepth, MaxDepth);
-            _subNodes[1] = CreateSubtree(topRight, MaxItems, newDepth, MaxDepth);
-            _subNodes[2] = CreateSubtree(bottomLeft, MaxItems, newDepth, MaxDepth);
-            _subNodes[3] = CreateSubtree(bottomRight, MaxItems, newDepth, MaxDepth);
+            var bounds = Bounds;
+
+            var subNodeSize = bounds.size * 0.5f;
+
+            for (var z = 0; z < 2; z++)
+            {
+                for (var y = 0; y < 2; y++)
+                {
+                    for (var x = 0; x < 2; x++)
+                    {
+                        var min = bounds.min + new Vector3(x * subNodeSize.x, y * subNodeSize.y, z * subNodeSize.z);
+                        var max = min + subNodeSize;
+
+                        var innerBounds = new Bounds();
+                        innerBounds.SetMinMax(min, max);
+                        _subNodes[z * 4 + y * 2 + x] = CreateSubtree(innerBounds, MaxItems, newDepth, MaxDepth);
+                    }
+                }
+            }
 
             for (var i = _items.Count - 1; i >= 0; i--)
             {
@@ -310,39 +318,57 @@ namespace Archon.SwissArmyLib.Partitioning
         private enum SubNodes
         {
             None = 0,
-            TopLeft = 1 << 0,
-            TopRight = 1 << 1,
-            BottomLeft = 1 << 2,
-            BottomRight = 1 << 3,
-            All = TopLeft | TopRight | BottomLeft | BottomRight
+            FrontTopLeft = 1 << 0,
+            FrontTopRight = 1 << 1,
+            FrontBottomLeft = 1 << 2,
+            FrontBottomRight = 1 << 3,
+            BackTopLeft = 1 << 4,
+            BackTopRight = 1 << 5,
+            BackBottomLeft = 1 << 6,
+            BackBottomRight = 1 << 7,
+
+            Front = FrontTopLeft | FrontTopRight | FrontBottomLeft | FrontBottomRight,
+            Back = ~Front,
+            Top = FrontTopLeft | FrontTopRight | BackTopLeft | BackTopRight,
+            Bottom = ~Top,
+            Left = FrontTopLeft | FrontBottomLeft | BackTopLeft | BackBottomLeft,
+            Right = ~Left,
+
+            All = Front | Back
         }
 
-        private SubNodes GetContainingNodes(Rect target)
+        private SubNodes GetContainingNodes(Bounds target)
         {
-            var subnodes = SubNodes.None;
+            var subnodes = SubNodes.All;
 
             var center = Bounds.center;
 
-            var left = target.xMin < center.x;
-            var right = target.xMax > center.x;
-            var bottom = target.yMin < center.y;
-            var top = target.yMax > center.y;
+            var front = target.min.z < center.z;
+            var back = target.max.z > center.z;
 
-            if (left)
-            {
-                if (top)
-                    subnodes |= SubNodes.TopLeft;
-                if (bottom)
-                    subnodes |= SubNodes.BottomLeft;
-            }
+            var bottom = target.min.y < center.y;
+            var top = target.max.y > center.y;
 
-            if (right)
-            {
-                if (top)
-                    subnodes |= SubNodes.TopRight;
-                if (bottom)
-                    subnodes |= SubNodes.BottomRight;
-            }
+            var left = target.min.x < center.x;
+            var right = target.max.x > center.x;
+
+            if (!front)
+                subnodes &= ~SubNodes.Front;
+
+            if (!back)
+                subnodes &= ~SubNodes.Back;
+
+            if (!bottom)
+                subnodes &= ~SubNodes.Bottom;
+
+            if (!top)
+                subnodes &= ~SubNodes.Top;
+
+            if (!left)
+                subnodes &= ~SubNodes.Left;
+
+            if (!right)
+                subnodes &= ~SubNodes.Right;
 
             return subnodes;
 
