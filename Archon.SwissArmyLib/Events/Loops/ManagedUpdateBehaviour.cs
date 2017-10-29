cce38083a463f2d18e1cbc198e467a2c928956d3
Casper Christiansen <casper.developer@gmail.com>
2017-10-29 17:39:12 +0100

    Made ManagedUpdateBehaviour support custom update loops

--- Archon.SwissArmyLib/Events/Loops/ManagedUpdateBehaviour.cs
+++ Archon.SwissArmyLib/Events/Loops/ManagedUpdateBehaviour.cs
@@ -36,7 +36,28 @@ namespace Archon.SwissArmyLib.Events.Loops
     }
 
     /// <summary>
+    /// Makes a <see cref="ManagedUpdateBehaviour"/> subclass get notified when a custom update loop ticks.
+    /// </summary>
+    public interface ICustomUpdateable
+    {
+        /// <summary>
+        /// Gets the event ids for the custom update loops that should be listened to.
+        /// </summary>
+        /// <returns>The event ids to listen for.</returns>
+        int[] GetCustomUpdateIds();
+
+        /// <summary>
+        /// Called whenever one of the custom update loops tick.
+        /// </summary>
+        /// <param name="eventId"></param>
+        void OnCustomUpdate(int eventId);
+    }
+
+    /// <summary>
     /// A subclass of MonoBehaviour that uses <see cref="ManagedUpdate"/> for update events.
+    /// 
+    /// To receive updates implement one or more of the appropriate interfaces: 
+    /// <see cref="IUpdateable"/>, <see cref="ILateUpdateable"/>, <see cref="IFixedUpdateable"/> and <see cref="ICustomUpdateable"/>.
     /// </summary>
     public abstract class ManagedUpdateBehaviour : MonoBehaviour, IEventListener
     {
@@ -52,6 +73,9 @@ namespace Archon.SwissArmyLib.Events.Loops
         private IUpdateable _updateable;
         private ILateUpdateable _lateUpdateable;
         private IFixedUpdateable _fixedUpdateable;
+        private ICustomUpdateable _customUpdateable;
+
+        private int[] _customUpdateIds;
 
         /// <summary>
         /// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
@@ -62,11 +86,13 @@ namespace Archon.SwissArmyLib.Events.Loops
             _updateable = this as IUpdateable;
             _lateUpdateable = this as ILateUpdateable;
             _fixedUpdateable = this as IFixedUpdateable;
+            _customUpdateable = this as ICustomUpdateable;
             // ReSharper restore SuspiciousTypeConversion.Global
 
             if (_updateable == null
                 && _lateUpdateable == null
-                && _fixedUpdateable == null)
+                && _fixedUpdateable == null
+                && _customUpdateable == null)
             {
                 Debug.LogWarning("This component doesn't implement any update interfaces.", this);
             }
@@ -87,6 +113,7 @@ namespace Archon.SwissArmyLib.Events.Loops
                 if (_updateable == null) _updateable = this as IUpdateable;
                 if (_lateUpdateable == null) _lateUpdateable = this as ILateUpdateable;
                 if (_fixedUpdateable == null) _fixedUpdateable = this as IFixedUpdateable;
+                if (_customUpdateable == null) _customUpdateable = this as ICustomUpdateable;
                 // ReSharper restore SuspiciousTypeConversion.Global
             }
 
@@ -112,12 +139,23 @@ namespace Archon.SwissArmyLib.Events.Loops
                 return;
             }
 
+            var executionOrder = ExecutionOrder;
+
             if (_updateable != null)
-                Loops.ManagedUpdate.OnUpdate.AddListener(this, ExecutionOrder);
+                ManagedUpdate.OnUpdate.AddListener(this, executionOrder);
             if (_lateUpdateable != null)
-                Loops.ManagedUpdate.OnLateUpdate.AddListener(this, ExecutionOrder);
+                ManagedUpdate.OnLateUpdate.AddListener(this, executionOrder);
             if (_fixedUpdateable != null)
-                Loops.ManagedUpdate.OnFixedUpdate.AddListener(this, ExecutionOrder);
+                ManagedUpdate.OnFixedUpdate.AddListener(this, executionOrder);
+
+            if (_customUpdateable != null)
+            {
+                if (_customUpdateIds == null)
+                    _customUpdateIds = _customUpdateable.GetCustomUpdateIds() ?? new int[0];
+
+                for (var i = 0; i < _customUpdateIds.Length; i++)
+                    ManagedUpdate.AddListener(_customUpdateIds[i], this, executionOrder);
+            }
 
             _isListening = true;
         }
@@ -131,11 +169,17 @@ namespace Archon.SwissArmyLib.Events.Loops
             }
 
             if (_updateable != null)
-                Loops.ManagedUpdate.OnUpdate.RemoveListener(this);
+                ManagedUpdate.OnUpdate.RemoveListener(this);
             if (_lateUpdateable != null)
-                Loops.ManagedUpdate.OnLateUpdate.RemoveListener(this);
+                ManagedUpdate.OnLateUpdate.RemoveListener(this);
             if (_fixedUpdateable != null)
-                Loops.ManagedUpdate.OnFixedUpdate.RemoveListener(this);
+                ManagedUpdate.OnFixedUpdate.RemoveListener(this);
+
+            if (_customUpdateable != null && _customUpdateIds != null)
+            {
+                for (var i = 0; i < _customUpdateIds.Length; i++)
+                    ManagedUpdate.RemoveListener(_customUpdateIds[i], this);
+            }
 
             _isListening = false;
         }
@@ -145,15 +189,26 @@ namespace Archon.SwissArmyLib.Events.Loops
         {
             switch (eventId)
             {
-                case Loops.ManagedUpdate.EventIds.Update:
+                case ManagedUpdate.EventIds.Update:
                     _updateable.OnUpdate();
                     return;
-                case Loops.ManagedUpdate.EventIds.LateUpdate:
+                case ManagedUpdate.EventIds.LateUpdate:
                     _lateUpdateable.OnLateUpdate();
                     return;
-                case Loops.ManagedUpdate.EventIds.FixedUpdate:
+                case ManagedUpdate.EventIds.FixedUpdate:
                     _fixedUpdateable.OnFixedUpdate();
                     return;
+            }
+
+            if (_customUpdateIds != null)
+            {
+                for (var i = 0; i < _customUpdateIds.Length; i++)
+                {
+                    if (_customUpdateIds[i] != eventId) continue;
+
+                    _customUpdateable.OnCustomUpdate(eventId);
+                    return;
+                }
             }
         }
     }
