43280a58629af5df73f849ad839fde86c79d43ab
Casper Lindschouw <casper.developer@gmail.com>
2019-10-20 18:57:03 +0200

    Revert "Fixed pooled GameObjects not getting activated when spawned"
    I'm a dumbass, I relied on my memory and fixed a non-issue.

--- Archon.SwissArmyLib/Pooling/GameObjectPool.cs
+++ Archon.SwissArmyLib/Pooling/GameObjectPool.cs
@@ -87,11 +87,8 @@ namespace Archon.SwissArmyLib.Pooling
 
             var gameObject = GetGameObject(obj);
             gameObject.transform.SetParent(null, false);
-
             if (_multiScene)
                 SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
-
-            gameObject.SetActive(true);
 
             return obj;
         }
